b2c38101567c3230b51de87771163f631d932d2b
balsipson <thirdi@gmail.com>
2022-12-21 23:51:23 +0530

    Updated gpt.ts code

---
+++ server/api/gpt.ts
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from "openai"
+
+export default defineEventHandler(async (event) => {
+    const config = useRuntimeConfig()
+    const configuration = new Configuration({
+        apiKey: config.OPENAI_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    // console.log(event)
+    const query = getQuery(event)
+    const body = await readBody(event)
+
+    // OpenAI settings
+    let model = "text-davinci-003";
+    let max_tokens = 256;
+    let temperature = 0.7;
+
+    try {
+        const prediction = await openai.createCompletion({
+            model: model,
+            prompt: query.prompt,
+            max_tokens: max_tokens,
+            temperature: temperature,
+        });
+
+        return { prediction }
+    } catch (error) {
+        console.log(error);
+        return { error }
+    }
+})
