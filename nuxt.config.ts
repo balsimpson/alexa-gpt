9a231bb20413beded293400f5f898de92128df2d
balsipson <thirdi@gmail.com>
2022-12-21 23:33:07 +0530

    removed firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,12 +12,12 @@ export default defineNuxtConfig({
       },
     css: ["~/assets/css/styles.css"],
     modules: ['@nuxtjs/tailwindcss'],
-    publicRuntimeConfig: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID
-    },
-    privateRuntimeConfig: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID
-    },
+    // publicRuntimeConfig: {
+    //   FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    //   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID
+    // },
+    // privateRuntimeConfig: {
+    //   FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    //   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID
+    // },
 })
